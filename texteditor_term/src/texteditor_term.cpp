8f7090aa2e41630fb15e6c42f0fda8f717dd33ea
maliozer <mehmetali.ozer@std.yeditepe.edu.tr>
2020-12-19 15:52:54 +0300

    start project

---
+++ texteditor_term/src/texteditor_term.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : texteditor_term.cpp
+// Author      : maliozer
+// Version     :
+// Copyright   : Copyright Mehmet Ali Özer
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "END OF PROGRAM" << endl; // prints END OF PROGRAM
+	return 0;
+}
